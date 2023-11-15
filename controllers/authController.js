1ca51ad5c00b464aa67c662307afe6f2bca921bc
Vasko <vbblajev@gmail.com>
2023-11-15 19:04:24 +0200

    Signup works in API (authentication done with JWTs)

---
+++ controllers/authController.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+const { promisify } = require('util');
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
+
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id) || signToken(user.id);
+
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+        secure: true
+    };
+
+    res.cookie('jwt', token, cookieOptions);
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    })
+}
+
+exports.login = catchAsync(async(req, res, next) => {
+
+});
+
+exports.signup = catchAsync(async(req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    newUser.password = undefined;
+
+    createSendToken(newUser, 201, res);
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/login', authController.login);
+router.post('/signup', authController.signup);
 
 router.route('/').get(userController.getAllUsers)
                  .post(userController.createUser);
